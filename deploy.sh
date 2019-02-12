8b04c8aaa6a7423d630eff7b38a88de837b4e5b2
Sasha Joseph <sasha.joseph@gmail.com>
2019-02-11 16:16:54 -0800

    fix: add /release-notes redirect on deploy and do not deploy deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,8 @@
 gitbook build \
 && aws s3 sync --delete --profile=haiku --exclude=".git/*" --acl=public-read ./_book/ s3://docs.haiku.ai/ \
 && aws configure set preview.cloudfront true \
+&& aws --profile=haiku s3api put-object --bucket=docs.haiku.ai \
+       --acl=public-read --website-redirect-location=/release-notes/index.html \
+       --key=release-notes \
 && aws --profile=haiku cloudfront create-invalidation --distribution-id=E1Z1KZNU21Z4PY --paths "/*" \
 && echo 'Deployed!'
