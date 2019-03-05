0aa5da4257c0e2ea8c9d536d394baa195b4d06b5
Sasha Joseph <sasha.joseph@gmail.com>
2019-03-05 13:38:30 -0800

    chore: update deploy command

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gitbook build \
+yarn gitbook build \
 && aws s3 sync --delete --profile=haiku-prod --exclude=".git/*" --acl=public-read ./_book/ s3://docs.haikuforteams.com/ \
 && aws configure set preview.cloudfront true \
 && aws --profile=haiku-prod s3api put-object --bucket=docs.haikuforteams.com \
